0aee21a27dbe5714d18d73c59e494f5f564ef128
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-09-24 16:16:55 +0900

    feat:create common header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template lang="pug">
   v-app#app
-    SideMenu
-    v-content.main-content
+    v-card(height="60px" flat tile)
+      v-app-bar(
+        :color="toolbarColor"
+        height="60px" prominent dense absolute dark
+        elevate-on-scroll scroll-target=".main-content"
+      )
+        v-toolbar-title DynamoDB Local Console
+    v-content.main-content.overflow-y-auto
+      SideMenu
       router-view
 </template>
 
@@ -14,11 +21,38 @@ import SideMenu from "@/components/sideMenu/SideMenu.vue"
 export default Vue.extend({
   components: {
     SideMenu
+  },
+  data() {
+    return {
+      toolbarColor: "#1867c0"
+    }
   }
 })
 </script>
 
 <style lang="scss">
+body {
+  margin: 0;
+
+  // スクロールバー非表示
+  ::-webkit-scrollbar {
+    width: 0px;
+  }
+
+  &::-webkit-scrollbar {
+    width: 0px;
+  }
+
+  // &::-webkit-scrollbar-track {
+  //   background-color: $scrollbar-track-color;
+  // }
+
+  // &::-webkit-scrollbar-thumb {
+  //   background: rgba($scrollbar-thumb-color, .6);
+  //   border-radius: 8px;
+  // }
+}
+
 #app {
   font-family: "Avenir", "Helvetica Neue", "Helvetica", "Arial", "Hiragino Sans",
     "ヒラギノ角ゴシック", YuGothic, "Yu Gothic", "メイリオ", Meiryo,
@@ -27,10 +61,5 @@ export default Vue.extend({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #303030;
-
-  .main-content {
-    background-color: #ededed;
-    padding-left: 256px !important;
-  }
 }
 </style>
