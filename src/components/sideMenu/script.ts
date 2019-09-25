f55f36ff8d588d3d3e13ac6dc5a047931d4b05c9
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-09-25 16:29:10 +0900

    feat:Create model for access by DynamoDNLocal

--- src/components/sideMenu/script.ts
+++ src/components/sideMenu/script.ts
@@ -1,18 +1,10 @@
 import { Component, Vue, Watch, Emit } from "vue-property-decorator"
-import { DynamoDB, Credentials } from "aws-sdk"
 import { CreateTableParam, KeyTypes } from "@/types"
-
-const db: DynamoDB = new DynamoDB({
-  endpoint: "http://localhost:8000",
-  region: "local",
-  credentials: new Credentials({
-    accessKeyId: "",
-    secretAccessKey: ""
-  })
-})
+import DynamoDBLocalModel from "@/models/dynamodb_local_model"
 
 @Component({})
 export default class SideMenu extends Vue {
+  public dynamoDBLocalModel: DynamoDBLocalModel = null
   public tableList: string[] = []
   public selectedTableName: string = ""
   public selectedTableIndex: number = null
@@ -53,8 +45,8 @@ export default class SideMenu extends Vue {
 
   public async created(): Promise<void> {
     try {
-      const tableList = await db.listTables().promise()
-      this.tableList = tableList.TableNames
+      this.dynamoDBLocalModel = new DynamoDBLocalModel()
+      this.tableList = await this.dynamoDBLocalModel.listTable()
     } catch (err) {
       console.error(err)
     }
@@ -63,37 +55,7 @@ export default class SideMenu extends Vue {
   // Create Table in DynamoDB local
   public async createTable(): Promise<void> {
     try {
-      console.log(this.createTableParam)
-      const param: DynamoDB.CreateTableInput = {
-        TableName: this.createTableParam.tableName,
-        AttributeDefinitions: [
-          {
-            AttributeName: this.createTableParam.hashKey,
-            AttributeType: this.createTableParam.hashKeyType
-          }
-        ],
-        KeySchema: [
-          {
-            AttributeName: this.createTableParam.hashKey,
-            KeyType: "HASH"
-          }
-        ],
-        ProvisionedThroughput: {
-          ReadCapacityUnits: 1,
-          WriteCapacityUnits: 1
-        }
-      }
-      if (this.createTableParam.rengeKey !== "") {
-        param.AttributeDefinitions.push({
-          AttributeName: this.createTableParam.rengeKey,
-          AttributeType: this.createTableParam.hashKeyType
-        })
-        param.KeySchema.push({
-          AttributeName: this.createTableParam.rengeKey,
-          KeyType: "RENGE"
-        })
-      }
-      await db.createTable(param).promise()
+      await this.dynamoDBLocalModel.createTable(this.createTableParam)
     } catch (err) {
       console.error(err)
     }
